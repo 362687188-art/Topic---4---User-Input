2642276671abab2d3793133140c9e34f18fab66f
Teddy Becker <362687188@GoTVDSB.ca>
2026-09-18 13:20:00 -0400

    started pricing

--- Topic - 4 - User Input/Program.cs
+++ Topic - 4 - User Input/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String Username;
+            int age;
+            double price;
+
+            Console.WriteLine("Enter username:");
+            Username = Console.ReadLine();
+            Console.WriteLine("Your username is: " + Username);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+            Console.WriteLine("Your age is " + age);
+            Console.ReadLine();
+
+            Console.WriteLine("What is the price?");
+            Double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine("The price is " + price.ToString("C"));
+
         }
     }
 }
